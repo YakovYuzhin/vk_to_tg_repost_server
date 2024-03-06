a19efbed3f16ad4473035c11960693be97e256d1
Yakov <gytersad07@gmail.com>
2024-03-06 18:12:54 +0300

    02. Added events endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/events')
+  getEvents(@Body() payload: any, @Res() res: any): any {
+    console.log(payload);
+
+    res.status(202).send('Accepted');
+    return 'OK'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const SmeeClient = require('smee-client');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.enableCors();
   await app.listen(3000);
 }
+
+// const smee = new SmeeClient({
+//   source: 'https://smee.io/T6URTJrcr5kZWAt4',
+//   target: 'http://localhost:3000/events',
+//   logger: console,
+// });
+
+//smee.start()
 bootstrap();
+
